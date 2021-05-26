af813ca502ab152f4d8a1472aec5b2c148c7b6f6
DESKTOP-I7RV353\renaldo <sao1barbara@gmail.com>
2021-05-26 10:13:52 -0300

    Segue projeto em andamento

---
+++ gestaodebombas/projetoposto/src/app/listaprodutos/listaprodutos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listaprodutos',
+  templateUrl: './listaprodutos.component.html',
+  styleUrls: ['./listaprodutos.component.css']
+})
+export class ListaprodutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
